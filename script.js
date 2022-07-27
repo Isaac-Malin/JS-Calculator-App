aa9e358314140a7b9871802be0edbe60c4699ee3
Isaac Malin <isaac.malin@mykenzie.snhu.edu>
2022-07-27 14:13:29 -0700

    Started pesudoCoding and touched up html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // first thing- User selects first number and store that number in const || let
 // Second thing- User selects operator and store that operator in const || let
 // thrid thing- User seclcts second number and store that number in const || let
+
