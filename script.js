75b623274a8f249fa1035ce48f00c12d16fcabb5
Isaac Malin <isaac.malin@mykenzie.snhu.edu>
2022-07-27 13:31:06 -0700

    finished html and css

---
+++ script.js
@@ -0,0 +1,3 @@
+// first thing- User selects first number and store that number in const || let
+// Second thing- User selects operator and store that operator in const || let
+// thrid thing- User seclcts second number and store that number in const || let
